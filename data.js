4bb844be83189afa963752c2ebe5fb82ca77a7de
Mohit1622 <mchauhan1622@gmail.com>
2026-03-03 21:04:00 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const allShayari = [
+    "--- Nayi Shayari Yahan Likhein ---",
+    "Aapki Sabse Nayi Shayari Sabse Upar",
+    
+    // Purani Shayari Niche Rahegi
+    "बेशक तुम जा चुके हो, पर तुम्हारा नाम आज भी, मेरे दिल की धड़कने तेज कर देता है।",
+    "अरे aapko to इज़्ज़त krni chahiye thi is खामोशी ki..! Hm to iske phichhe aapki औकात छिपाये baithe the..!!"
+];
+
