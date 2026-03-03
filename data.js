0d9a84bdfabbf42cdafb5268273e5f500d55421f
Mohit1622 <mchauhan1622@gmail.com>
2026-03-03 21:10:38 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,5 @@
 const allShayari = [
     // --- Nayi Shayari Sabse Upar Yahan Likhein ---
-    "Yahan apni nayi shayari likhein",
     
     // --- Aapka Purana Sara Data Niche Hai ---
     "बेशक तुम जा चुके हो, पर तुम्हारा नाम आज भी, मेरे दिल की धड़कने तेज कर देता है।",
