81021e2aa243eddac517d70c409dc09269e61a15
Mohit1622 <mchauhan1622@gmail.com>
2026-03-05 19:30:55 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const siteConfig = {
+    // 1. Social Media Links (Yahan apni link daalein)
+    instagram: "https://www.instagram.com/mohitrajput_1622?igsh=MWRicnU4OWhueGRoZQ==",
+    facebook: "https://www.facebook.com/profile.php?id=100015466385333",
+    whatsapp: "917018645382", // Apna number 91 ke sath
+    
+    // 2. Owner Details
+    email: "mchauhan1622@gmail.com",
+    
+    // 3. Auto Event Settings
+    autoEvents: true // Ise true rakhne par Holi/Diwali themes apne aap aayengi
+};
