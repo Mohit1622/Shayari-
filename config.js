4bef1cd02b2c7ea60d36ba511828b110b271b4a7
Mohit1622 <mchauhan1622@gmail.com>
2026-03-06 09:53:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,7 @@
-const siteConfig = {
-    // 1. Social Media Links (Yahan apni link daalein)
+const SiteConfig = {
+    whatsappNumber: "917018645382", // Apna 12 digit number daalein (Country code ke saath)
     instagram: "https://www.instagram.com/mohitrajput_1622?igsh=MWRicnU4OWhueGRoZQ==",
     facebook: "https://www.facebook.com/profile.php?id=100015466385333",
-    whatsapp: "917018645382", // Apna number 91 ke sath
-    
-    // 2. Owner Details
-    email: "mchauhan1622@gmail.com",
-    
-    // 3. Auto Event Settings
-    autoEvents: true // Ise true rakhne par Holi/Diwali themes apne aap aayengi
+    upiId: "mchauhan1622@oksbi", // Apna sahi UPI ID yahan daalein
+    siteName: "The Shayari Hub"
 };
