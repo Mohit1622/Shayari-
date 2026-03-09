b3bc535c2117d5e62b6c90366539f4a2e837fe1a
Mohit1622 <mchauhan1622@gmail.com>
2026-03-09 19:27:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,7 @@ const SiteConfig = {
     instagram: "https://www.instagram.com/mohitrajput_1622?igsh=MWRicnU4OWhueGRoZQ==",
     facebook: "https://www.facebook.com/profile.php?id=100015466385333",
     upiId: "mchauhan1622@oksbi", // Apna sahi UPI ID yahan daalein
+    email: " mchauhan1622@gmail.com", 
+    address: "Hamirpur, Himachal Pradesh",
     siteName: "The Shayari Hub"
 };
